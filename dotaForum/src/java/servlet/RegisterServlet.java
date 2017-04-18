97f7c968db14f7d9ce69b26d76a7516b75eae84a
EvanJoshua <vanzjo@gmail.com>
2017-04-18 14:03:35 +0700

    tes

--- dotaForum/src/java/servlet/RegisterServlet.java
+++ dotaForum/src/java/servlet/RegisterServlet.java
@@ -19,9 +19,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.User;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 /**
  *
